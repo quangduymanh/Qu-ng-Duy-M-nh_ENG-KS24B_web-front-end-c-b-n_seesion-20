0d8ce4158a3157eb46a3579ce5d4b3bfbb0b0a52
quangduymanh <quangduymanh4@gmail.com>
2025-03-05 08:49:27 +0700

    Update seesion 20-bài 01.js

--- seesion 20-bài 01.js
+++ seesion 20-bài 01.js
@@ -1,1 +1,8 @@
-let  number = +prompt ("mời nhập vào số cộng dồn");
+let N =+prompt("Nhập số nguyên dương N:");
+let sum = 0;
+
+for (let i = 1; i <= N; i++) {
+    sum += i;
+}
+
+console.log("Tổng từ 1 đến " + N + " là: " + sum);
